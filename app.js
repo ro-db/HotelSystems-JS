022506c67f8734907baaf62ca12ff8749285aa69
OmarAlmo <omar.almoallim@gmail.com>
2019-03-25 18:18:51 -0400

    minor addtions to connect to db

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const path = require('path');
+const { Client, Pool } = require('pg');
+require('dotenv').config();
 
 var engines = require('consolidate');
 
 // Initiate app
 const app = express();
+var PORT = process.env.PORT || 3000;
+
+// Create connection
+const connectionString = process.env.CONNECTION_STRING;
+
+const pool = new Pool({
+  connectionString: connectionString
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
+
+const client = new Client({
+  connectionString: connectionString
+});
+client.connect();
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  client.end();
+});
 
 // Set app engine
 app.set('views', __dirname + '/views');
@@ -17,6 +42,6 @@ app.get('/', function(req, res) {
 });
 
 // Start server
-app.listen(3000, function() {
-  console.log('Server started on port 3000');
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`);
 });
