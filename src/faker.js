2c66a7abf49cfe814edf1ebca48f30f2813ca731
OmarAlmo <omar.almoallim@gmail.com>
2019-03-31 14:04:53 -0400

    able to add hotels and hotel chains

--- src/faker.js
+++ src/faker.js
@@ -22,7 +22,7 @@ var hotelChains = [],
 function createHotelChains() {
   for (var i = 1; i < 6; i++) {
     hotelChainID = i;
-    numberOfHotels = Math.floor(Math.random() * 20) + 5;
+    numberOfHotels = Math.floor(Math.random()) + 5;
     address = faker.address.streetAddress();
     email = faker.internet.exampleEmail();
     phoneNumber = faker.phone.phoneNumberFormat().toString();
@@ -32,7 +32,7 @@ function createHotelChains() {
       [hotelChainID, numberOfHotels, address, email, phoneNumber]
     )
       .then(() => {
-        console.log('SUCCESSS');
+        console.log('SUCCESSS: ADDED HOTEL CHAIN');
       })
       .catch(error => {
         console.log(error, 'ERROR!!');
@@ -46,52 +46,58 @@ function createHotelChains() {
     //   phoneNumber
     // );
     // hotelChains.push(hc);
-
-    console.log(hotelChainID, numberOfHotels, address, email, phoneNumber);
     console.log(`created hotel chain ${i}`);
   }
 }
 
 function createHotels() {
+  var pk = 0;
   for (var i = 1; i < 9; i++) {
-    for (var j = 1; j < 6; j++) hotelID = j;
-    hotelChainID = j;
-    numberOfRooms = Math.floor(Math.random() * 5) + 3;
-    phoneNumber = faker.phone.phoneNumberFormat().toString();
-    address = faker.address.streetAddress();
-    email = faker.internet.exampleEmail();
-    numberOfStars = Math.floor(Math.random() * 3) + 3;
+    hotelID = i;
+    for (var j = 1; j < 6; j++) {
+      hotelChainID = j;
+      numberOfRooms = Math.floor(Math.random() * 7) + 3;
+      phoneNumber = faker.phone.phoneNumberFormat().toString();
+      address = faker.address.streetAddress();
+      email = faker.internet.exampleEmail();
+      numberOfStars = Math.floor(Math.random() * 3) + 3;
+      pk++;
+
+      db.none(
+        'INSERT INTO hotel(pk, hotel_id, hotel_chain_id, number_of_rooms, phone_number, address, email, stars) VALUES ($1, $2, $3, $4, $5, $6, $7,$8);',
+        [
+          pk,
+          hotelID,
+          hotelChainID,
+          numberOfRooms,
+          phoneNumber,
+          address,
+          email,
+          numberOfStars
+        ]
+      )
+        .then(() => {
+          console.log('SUCCESSS: ADDED HOTEL');
+        })
+        .catch(error => {
+          console.log(error, 'ERROR!!');
+        });
+    }
+    // var hotel = new Hotel(
+    //   hotelID,
+    //   hotelChainID,
+    //   numberOfRooms,
+    //   phoneNumber,
+    //   address,
+    //   email,
+    //   numberOfStars
+    // );
+    // hotels.push(hotel);
   }
-
-  db.none(
-    'INSERT INTO hotel(hotel_id, hotel_chain_id, number_of_rooms, phone_number, address, email, stars) VALUES ($1, $2, $3, $4, $5, $6, $7);',
-    [
-      hotelID,
-      hotelChainID,
-      numberOfRooms,
-      phoneNumber,
-      address,
-      email,
-      numberOfStars
-    ]
-  )
-    .then(() => {
-      console.log('SUCCESSS');
-    })
-    .catch(error => {
-      console.log(error, 'ERROR!!');
-    });
-
-  // var hotel = new Hotel(
-  //   hotelID,
-  //   hotelChainID,
-  //   numberOfRooms,
-  //   phoneNumber,
-  //   address,
-  //   email,
-  //   numberOfStars
-  // );
-  // hotels.push(hotel);
 }
+
 createHotels();
-(module.exports = createHotels()), createHotelChains();
+createHotelChains();
+
+module.exports = createHotels();
+module.exports = createHotelChains();
